0b3ee2dffdd2707dea1bb06fdf235bed31cd0939
grigorii <gr.rassadnikov@mail.ru>
2023-04-11 16:16:30 +0300

    Intial commit

---
+++ GoInvest/GoInvest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoInvest
+//
+//  Created by Grigorii Rassadnikov on 11.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
