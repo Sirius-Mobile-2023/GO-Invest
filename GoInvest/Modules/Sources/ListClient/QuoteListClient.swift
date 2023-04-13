7d80d17f3cf8e98592f4db574e82f9b146ce8d56
gr-rassadnikov <gr>
2023-04-13 15:00:41 +0300

    ref name for public func

--- GoInvest/Modules/Sources/ListClient/QuoteListClient.swift
+++ GoInvest/Modules/Sources/ListClient/QuoteListClient.swift
@@ -3,7 +3,7 @@ import Foundation
 
 public final class QuoteListClient: ListProvider {
     public func quoteList(search: SearchForList, completion: (_: Result<[Quote], Error>) -> Void) {
-        let quoteListResult = getQuoteListByUrl(search: search)
+        let quoteListResult = getQuoteListBySearch(search: search)
         let quoteList: Result<[Quote], Error>
         switch quoteListResult {
         case let .success(quoteLR):
@@ -14,7 +14,7 @@ public final class QuoteListClient: ListProvider {
         completion(quoteList)
     }
 
-    private func getQuoteListByUrl(search _: SearchForList) -> Result<[QuoteResult], Error> {
+    private func getQuoteListBySearch(search _: SearchForList) -> Result<[QuoteResult], Error> {
         // make get response to server
         return Result.failure(fatalError("Write code!!! QuoteListClient.getQuoteListByUrl"))
     }
