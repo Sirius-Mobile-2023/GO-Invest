520b03c2359efbbae834eae53e3d50871904e660
Kirill P <kpadalitsa@gmail.com>
2023-04-14 21:37:05 +0300

    add a separate function to update the appearance of buttons

--- GoInvest/Modules/Sources/QuoteDetail/QuoteDetailViewController.swift
+++ GoInvest/Modules/Sources/QuoteDetail/QuoteDetailViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 public class QuoteDetailViewController: UIViewController {
     
     private lazy var control: TimeIntervalsControl = {
-        let control = TimeIntervalsControl(intervals: ["1D", "7D", "1M", "3M", "1Y"], selectedSegmentIndex: 4)
+        let control = TimeIntervalsControl(intervals: ["1D", "7D", "1M", "3M", "1Y"])
         control.translatesAutoresizingMaskIntoConstraints = false
         return control
     }()
@@ -19,7 +19,7 @@ public class QuoteDetailViewController: UIViewController {
         NSLayoutConstraint.activate([
             control.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 45),
             control.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -45),
-            control.heightAnchor.constraint(equalToConstant: 40),
+            control.heightAnchor.constraint(equalToConstant: 45),
             control.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
         ])
     }
--- GoInvest/Modules/Sources/QuoteDetail/TimeIntervalsControl/TimeIntervalsControl.swift
+++ GoInvest/Modules/Sources/QuoteDetail/TimeIntervalsControl/TimeIntervalsControl.swift
@@ -3,59 +3,70 @@ import AudioToolbox
 
 class TimeIntervalsControl: UIView {
 
-    private var selectedSegmentIndex: Int
-    private var segments = [IntervalButton]()
+    var selectedSegmentIndex: Int {
+        didSet {
+            updateSegments(
+                selectedSegmentIndex: selectedSegmentIndex,
+                oldSelectedSegmentIndex: oldValue
+            )
+        }
+    }
+    
+    private var segments = [UIButton]()
     
     private var selectorView: UIView = {
         let view = UIView()
         view.layer.cornerRadius = Constants.cornerRadius
-        view.backgroundColor = Constants.selectBackgroundColor
+        view.backgroundColor = Constants.selectedBackgroundColor
         view.translatesAutoresizingMaskIntoConstraints = false
-       return view
+        return view
     }()
     
     private let stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .center
-        stackView.spacing = 10
         stackView.distribution = .equalSpacing
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    init(
+    init (
         intervals: [String],
-        selectedSegmentIndex: Int
-    ){
+        selectedSegmentIndex: Int = Constants.defaultSelectedSegmentIndex
+    ) {
         if selectedSegmentIndex >= intervals.count || selectedSegmentIndex < 0 {
             self.selectedSegmentIndex = Constants.defaultSelectedSegmentIndex
         } else {
             self.selectedSegmentIndex = selectedSegmentIndex
         }
         super.init(frame: .zero)
-        self.setupLayout()
-        self.setupUI(intervals: intervals)
+        setupLayout()
+        setupUI(intervals: intervals)
+    }
+    
+    required public init? (coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func setupLayout() {
-        self.backgroundColor = .clear
-        self.addSubview(selectorView)
-        self.addSubview(stackView)
+        backgroundColor = .clear
+        addSubview(selectorView)
+        addSubview(stackView)
     }
     
     private func setupUI(intervals: [String]) {
         for (index, interval) in intervals.enumerated() {
-            let button = IntervalButton(
+            let button = UIButton(
                 title: interval,
                 titleColor: Constants.defaultTitleColor,
                 backgroundColor: Constants.defaultBackgroundColor,
-                borderColor: Constants.selectBackgroundColor,
+                borderColor: Constants.selectedBackgroundColor,
                 cornerRadius: Constants.cornerRadius,
                 borderWidth: Constants.borderWidth
             )
             
-            if index == self.selectedSegmentIndex {
+            if index == selectedSegmentIndex {
                 button.setTitleColor(Constants.selectTitleColor, for: .normal)
             }
             
@@ -79,38 +90,38 @@ class TimeIntervalsControl: UIView {
         ])
     }
     
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    @objc func buttonTapped(button: UIButton) {
-        for (index, btn) in segments.enumerated() {
-            btn.isSelected = false
-            if btn.tag == button.tag {
-                btn.isSelected.toggle()
-                UIView.animate(
-                    withDuration: 0.2,
-                    delay: 0,
-                    options: .curveEaseOut,
-                    animations: {
-                        self.selectorView.frame = btn.frame
-                        self.segments[self.selectedSegmentIndex].backgroundColor = Constants.defaultBackgroundColor
-                        self.segments[self.selectedSegmentIndex].setTitleColor(Constants.defaultTitleColor, for: .normal)
-                        btn.backgroundColor = Constants.selectBackgroundColor
-                        btn.setTitleColor(Constants.selectTitleColor, for: .normal)
-                    }, completion: { _ in
-                        self.selectedSegmentIndex = index
-                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                })
-            }
+    private func updateSegments(selectedSegmentIndex: Int, oldSelectedSegmentIndex: Int) {
+        segments.forEach { button in
+            button.isSelected = false
         }
+        segments[selectedSegmentIndex].isSelected.toggle()
+        UIView.animate(
+            withDuration: 0.2,
+            delay: 0,
+            options: .curveEaseOut,
+            animations: {
+                self.selectorView.frame = self.segments[selectedSegmentIndex].frame
+                self.segments[oldSelectedSegmentIndex].backgroundColor = Constants.defaultBackgroundColor
+                self.segments[oldSelectedSegmentIndex].setTitleColor(Constants.defaultTitleColor, for: .normal)
+                self.segments[selectedSegmentIndex].backgroundColor = Constants.selectedBackgroundColor
+                self.segments[selectedSegmentIndex].setTitleColor(Constants.selectTitleColor, for: .normal)
+            }, completion: { _ in
+                UIImpactFeedbackGenerator(style: .light).impactOccurred()
+            })
+    }
+}
+
+extension TimeIntervalsControl {
+    @objc
+    private func buttonTapped(button: UIButton) {
+        selectedSegmentIndex = button.tag
     }
 }
 
 private extension TimeIntervalsControl {
     struct Constants {
         static let defaultSelectedSegmentIndex = 0
-        static let selectBackgroundColor = UIColor.gray
+        static let selectedBackgroundColor = UIColor.gray
         static let defaultBackgroundColor = UIColor.clear
         static let selectTitleColor = UIColor.white
         static let defaultTitleColor = UIColor.gray
@@ -118,3 +129,25 @@ private extension TimeIntervalsControl {
         static let borderWidth: CGFloat = 1
     }
 }
+
+private extension UIButton {
+    convenience init(
+        title: String,
+        titleColor: UIColor,
+        backgroundColor: UIColor,
+        borderColor: UIColor,
+        cornerRadius: CGFloat,
+        borderWidth: CGFloat
+    ) {
+        self.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderWidth = borderWidth
+        self.layer.cornerRadius = cornerRadius
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+
